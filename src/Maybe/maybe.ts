9d3ab2259e43de9e02c121ce277f19c1b80e0dac
Ryan Durling <ryan.p.durling@gmail.com>
2022-08-31 18:39:59 -0500

    be a little more explicit for the maybe types in the implementation

--- src/Maybe/maybe.ts
+++ src/Maybe/maybe.ts
@@ -1,7 +1,7 @@
-import {Maybe} from "./types";
+import {Maybe, Nothing, Some} from "./types";
 
 const maybe= <T>(thing?: T, isSomething?: boolean): Maybe<T> => (thing === isSomething) as unknown as Maybe<T>
-const some = <T>(thing?: T): Maybe<T> => thing as Maybe<T>
-const nothing = <T>(thing?: T): Maybe<T> => thing as Maybe<T>
+const some = <T>(thing?: T): Some<T> => thing as Some<T>
+const nothing = (): Nothing => ({}) as Nothing
 
 export {maybe, some, nothing}
