61161955280c277bac9298c42d1d9ce2f39f396f
Ryan Durling <ryan.p.durling@gmail.com>
2022-09-03 10:21:09 -0500

    gve people more of a place to start

--- src/Maybe/maybe.ts
+++ src/Maybe/maybe.ts
@@ -2,9 +2,29 @@ import {Maybe, Nothing, Some} from "./types";
 
 // TODO: Implement Maybe
 const maybe= <T>(thing?: T, isSomething?: boolean): Maybe<T> => (thing === isSomething) as unknown as Maybe<T>
+
 // TODO: Implement Some
-const some = <T>(thing: T): Some<T> => thing as Some<T>
+const some = <T>(thing: T): Some<T> => ({
+    isNothing: undefined,
+    orNull: () => undefined,
+    orElse: () => undefined,
+    map: () => undefined,
+    mBind: () => undefined,
+    or: () => undefined,
+    toResult: () => undefined,
+    inspect: () => undefined
+}) as unknown as Some<T>
+
 // TODO: Implement Nothing
-const nothing = (): Nothing => ({}) as Nothing
+const nothing = (): Nothing => ({
+    isNothing: undefined,
+    orNull: () => undefined,
+    orElse: () => undefined,
+    map: () => undefined,
+    mBind: () => undefined,
+    or: () => undefined,
+    toResult: () => undefined,
+    inspect: () => undefined
+}) as unknown as Nothing
 
 export {maybe, some, nothing}
